38ca97e046777e7318b5e3c7f32d8cd2ad5b2b7d
nishant <nishant.singh87@gmail.com>
2021-03-17 20:02:38 +0530

    Create a unit test for sorter.sort

---
+++ src/main/java/Sorter.java
@@ -0,0 +1,5 @@
+public class Sorter {
+    public int[] sort(int[] array){
+        return new int[]{1, 2 ,3, 4, 5};
+    }
+}
---
+++ src/test/java/HelloSorterTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class HelloSorterTest {
+    private Sorter sorter = new Sorter();
+
+    @Test
+    void shouldSayHelloWorld() {
+        int[] input = new int[]{5, 3, 2 ,1, 4};
+        int[] expected = new int[]{1, 2 ,3, 4, 5};
+        assertArrayEquals(sorter.sort(input), expected);
+    }
+}
