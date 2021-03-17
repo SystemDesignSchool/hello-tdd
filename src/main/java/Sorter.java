8933cfdfd7fc4edb4ba789e7056344de04b59f8c
Rishabh <unit.rishabh@gmail.com>
2021-03-17 20:10:45 +0530

    added logic for sorter

--- src/main/java/Sorter.java
+++ src/main/java/Sorter.java
@@ -1,5 +1,17 @@
 public class Sorter {
     public int[] sort(int[] array){
-        return new int[]{1, 2 ,3, 4, 5};
+
+        int temp = 0;
+        for(int i = 0; i < array.length - 1; i++){
+            for (int j = i+1; j < array.length; j++){
+                if (array[j] < array[i]){
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+
+        return array;
     }
 }
