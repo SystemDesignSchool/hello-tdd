704f09694bc46a5d792bf26e7fb936f2995009cb
Rishabh <unit.rishabh@gmail.com>
2021-03-18 19:41:14 +0530

    breaking user inpout into command and values

--- src/main/java/io/rishabh/app/calculator/CalculatorApp.java
+++ src/main/java/io/rishabh/app/calculator/CalculatorApp.java
@@ -3,7 +3,12 @@ package io.rishabh.app.calculator;
 public class CalculatorApp {
 
     public String execute(String command){
-        if(command.startsWith("add")){
+
+        String[] inputFromUser  = command.split(" ");
+        String inputCommand = inputFromUser[0];
+        String values = inputFromUser[1];
+
+        if (inputCommand.equals("add")){
             return "90";
         }
         return "Invalid command";
