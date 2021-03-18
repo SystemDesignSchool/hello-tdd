575834545798d159781a4a98bc5af5e92e4ce136
Rishabh <unit.rishabh@gmail.com>
2021-03-18 20:38:07 +0530

    refactor variable names

--- src/main/java/io/rishabh/app/calculator/CalculatorApp.java
+++ src/main/java/io/rishabh/app/calculator/CalculatorApp.java
@@ -4,35 +4,34 @@ public class CalculatorApp {
 
     public String execute(String command){
 
-        String[] inputFromUser  = command.split(" ");
-        String inputCommand = inputFromUser[0];
-        String values = inputFromUser[1];
-        int product = 1;
+        String[] inputs  = command.split(" ");
+        String action = inputs[0];
+        String values = inputs[1];
 
-        if (inputCommand.equalsIgnoreCase("add")){
-            return addNumbers(values);
+        if (action.equalsIgnoreCase("add")){
+            return add(values);
         }
 
-        if(inputCommand.equalsIgnoreCase("multiply")){
-            return multiplyNumbers(values);
+        if(action.equalsIgnoreCase("multiply")){
+            return multiply(values);
         }
         return "Invalid command";
     }
 
-    private String addNumbers(String values){
+    private String add(String values){
         int sum = 0;
         String[] eachValue = values.split(",");
-        for(String myValue : eachValue){
-            sum += Integer.parseInt(myValue);
+        for(String obtainedValue : eachValue){
+            sum += Integer.parseInt(obtainedValue);
         }
         return Integer.toString(sum);
     }
 
-    private String multiplyNumbers(String values){
+    private String multiply(String values){
         int product = 1;
         String[] eachValue = values.split(",");
-        for(String myValue : eachValue){
-            product = product * Integer.parseInt(myValue);
+        for(String obtainedValue : eachValue){
+            product = product * Integer.parseInt(obtainedValue);
         }
         return Integer.toString(product);
     }
